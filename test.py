5a4eea2d87319e6dedb9e024c039a22663254609
Jebran Irshad Syed <102875784+jebran0105@users.noreply.github.com>
2026-05-28 09:00:33 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import asyncio
 from pydoll.browser.chromium import Chrome
+from pydoll.browser.options import Options
 
 async def main():
-    async with Chrome() as browser:
+    options = Options()
+    options.add_argument('--user-data-dir=PASTE_PROFILE_PATH_HERE')
+    options.add_argument('--profile-directory=Default')
+
+    async with Chrome(options=options) as browser:
         tab = await browser.start()
         await tab.go_to('https://vcheck.ttc.lacounty.gov')
         await asyncio.sleep(3)
